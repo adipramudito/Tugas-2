5c5f7e5db1d171b6bb1e3d866449d4c83edc050d
adipramudito <adi.pramudito@gmail.com>
2021-02-27 12:59:29 +0700

    Tugas-2

---
+++ Soal-1.py
@@ -0,0 +1,46 @@
+print("Selamat Datang!")
+menu=[]
+menu0="---Menu---"
+menu1="1. Daftar Kontak"
+menu2="2. Tambah Kontak"
+menu3="3. Keluar"
+menu.append(menu0)
+menu.append(menu1)
+menu.append(menu2)
+menu.append(menu3)
+for y in menu:
+    print(y)
+
+kontak={}
+loop=0
+while loop<4:
+    loop=0
+    x=int(input("### Pilihan menu: "))
+    
+    if x==1:
+        if kontak=={}:
+            print("XXX Kontak kosong XXXX")
+        else:    
+            for nama,no_hp in kontak.items():
+                print(nama,no_hp)
+        
+    elif x==2:
+        nama=input("Nama: ")
+        no_hp=input("Nomor HP: ")
+        kontak[nama]=no_hp
+        print("***Kontak berhasil ditambahkan***")
+
+    elif x<1 or x>3:
+        print("Menu tidak tersedia.")
+
+    else:
+        print("---Terima kasih, keluar program---")
+        loop=5
+    
+    for y in menu:
+        print("\n",y,"\n")
+    
+
+
+
+
